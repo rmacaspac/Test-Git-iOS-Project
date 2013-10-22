2bfb0d4b88a4d12f91ba9d5dda54dec52048bd26
Ryan Macaspac <rmacaspac@gmail.com>
2013-10-21 22:28:43 -0700

    added test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Ryan Macaspac on 10/21/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Ryan Macaspac on 10/21/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
